6dc6a5238a195df4426e81734a5c05cb9ae914da
Valentin Vergara Hidd <valentinvergara@gmail.com>
2020-09-30 19:20:09 -0400

    added keyboard shotcut to zathura

--- config.h
+++ config.h
@@ -110,6 +110,7 @@ static Key keys[] = {
 	{ MODKEY,             		XK_Return, 	spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_w,	   	spawn,          SHCMD("brave")},
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("emacs")},
+	{ MODKEY,			XK_z,		spawn,		SHCMD("zathura")},
 	{ MODKEY,                       XK_b,      	togglebar,      {0} },
 	{ MODKEY,                       XK_j,      	focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      	focusstack,     {.i = -1 } },
