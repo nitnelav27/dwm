89da9b2210b7867f6635c4cc6f01617e93ea11d1
Valentin Vergara Hidd <valentinvergara@gmail.com>
2021-03-01 02:03:00 -0500

    updated google

--- config.def.h
+++ config.def.h
@@ -111,7 +111,7 @@ static Key keys[] = {
 	/* modifier                     key        	function        argument */
 	{ MODKEY,                       XK_p,      	spawn,          {.v = dmenucmd } },
 	{ MODKEY,             		XK_Return, 	spawn,          {.v = termcmd } },
-	{ MODKEY,			XK_w,	   	spawn,          SHCMD("google-chrome")},
+	{ MODKEY,			XK_w,	   	spawn,          SHCMD("google-chrome-stable")},
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("emacs")},
 	{ MODKEY,                       XK_b,      	togglebar,      {0} },
 	{ MODKEY,                       XK_j,      	focusstack,     {.i = +1 } },
